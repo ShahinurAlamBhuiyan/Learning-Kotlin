54e342f13f7c85a1fb94c4c4285cdbf1ede5cc40
ShahinurAlamBhuiyan <shahinur.alam.bhuiyan01@gmail.com>
2023-08-08 18:06:39 +0600

    about [variables]

--- src/main/kotlin/variables.kt
+++ src/main/kotlin/variables.kt
@@ -1,4 +1,16 @@
  fun main() {
-    var username:String = "Shahinur Alam Bhuiyan";
-    println("Hello $username");
+    var username:String = "Shahinur Alam Bhuiyan"; // var can be changed by overriding
+     username = "Shahin Bhuiyan";
+//     username = 45; // can't store another datatype.
+
+     val age:Int = 22; // val can't be changed by overriding
+     // age = 50; // getting error.
+
+     // we can use variable without declaring the type
+
+     var fatherName = "Shah Alam Bhuiyan";
+     var fatherAge = age;
+
+
+    println("Hello $username your age is $age");
  }
