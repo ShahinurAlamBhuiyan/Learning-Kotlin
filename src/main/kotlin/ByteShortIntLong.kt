c2c4fb5343da84492f4c66176ab04d9675145ea4
ShahinurAlamBhuiyan <shahinur.alam.bhuiyan01@gmail.com>
2023-08-08 18:15:44 +0600

    about [IntegerTypes]

---
+++ src/main/kotlin/ByteShortIntLong.kt
@@ -0,0 +1,30 @@
+fun main(){
+    var number = 22; // default datatype is int
+    val maximumLongValue: Long = Long.MAX_VALUE;
+    val minimumLongValue: Long = Long.MIN_VALUE;
+
+    println("Maximum Long Value: $maximumLongValue");
+    println("Minimum Long Value: $minimumLongValue");
+
+
+    val maximumIntNumber = Int.MAX_VALUE;
+    val minimumIntNumber = Int.MIN_VALUE;
+
+    println("Maximum Int Number: ${Int.MAX_VALUE}");
+    println("Minimum Int Number: $minimumIntNumber");
+    // number = 2147483648; // Maximum Int Number is 2147483647.
+
+
+    val maximumShortValue: Short = Short.MAX_VALUE;
+    val minimumShortValue: Short = Short.MIN_VALUE;
+
+    println("Maximum Short Value: $maximumShortValue");
+    println("Minimum Short Value: $minimumShortValue");
+
+
+    val maximumByteValue: Byte = Byte.MAX_VALUE;
+    val minimumByteValue: Byte = Byte.MIN_VALUE;
+
+    println("Maximum Byte Value: $maximumByteValue");
+    println("Minimum Byte Value: $minimumByteValue");
+}
